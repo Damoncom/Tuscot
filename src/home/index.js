b8c7d116aa0a9371163eac9529a8401433799230
Damon <2874681789@qq.com>
2024-10-16 18:30:05 +0800

    安装redux

--- src/home/index.js
+++ src/home/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from 'react';
+import { useLocation } from 'react-router';
 import Navigator from '../components/navigator/index';
 import SideBar from '../components/sideBar/index';
 import Dashboard from '../components/main/dashboard/index';
@@ -6,7 +7,7 @@ import Projects from '../components/main//projects/index';
 import MyTask from '../components/main/myTask/index';
 import Message from '../components/main/message/index';
 import Analytics from '../components/main/analytics/index';
-import { useLocation } from 'react-router';
+import { Counter } from '../features/counter/Counter';
 
 export default function Home() {
   // 获取sideBar传参
@@ -23,6 +24,7 @@ export default function Home() {
       {nowIndex.index === 2 && <MyTask />}
       {nowIndex.index === 3 && <Message />}
       {nowIndex.index === 4 && <Analytics />}
+      <Counter />
     </>
   );
 }
